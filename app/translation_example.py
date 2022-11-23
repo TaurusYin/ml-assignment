5cf39dc4d4308714c663ace0f91b35d844c209fd
yineric <yinzheng163@gmail.com>
2022-11-24 04:45:30 +0800

    commit

--- app/translation_example.py
+++ app/translation_example.py
@@ -5,9 +5,7 @@ import sentencepiece
 model = M2M100ForConditionalGeneration.from_pretrained('facebook/m2m100_418M')
 tokenizer = M2M100Tokenizer.from_pretrained('facebook/m2m100_418M', src_lang="en", tgt_lang="fr")
 
-
 if __name__ == "__main__":
-
     src_text = "Life is like a box of chocolates."
     tgt_lang = "人生はチョコレートの箱のようなものだ。"
     src_text_records = [src_text, src_text]
